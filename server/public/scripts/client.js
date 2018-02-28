cebda5878bb6cf68e94467efe88a94e3f381c24d
liz elton <eeeqqqeee@gmail.com>
2018-02-27 19:14:54 -0600

    add account views

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -10,6 +10,12 @@ app.config(function($routeProvider){
         }).when('/fail', {
             templateUrl: '/views/fail.html',
             controller: 'WIWController as vm'
+        }).when('/10000', {
+            templateUrl: '/views/abc.html',
+            controller: 'WIWController as vm'
+        }).when('/12345', {
+            templateUrl: '/views/123.html',
+            controller: 'WIWController as vm'
         }).otherwise({
             template: '<h1>404</h1><p>no bueno</p>'
         });
@@ -20,6 +26,50 @@ app.controller('WIWController', function(){
     var self = this;
 
     self.message = "howdy yall";
+
+    self.data = {
+        "login": {
+          "id": 5,
+          "first_name": "Goldie",
+          "last_name": "Wilson",
+          "email": "goldiewilson@hillvalleygov.org",
+          "token": "ilovemyboss"
+        },
+        "users": [
+          {
+            "id": 4364,
+            "login_id": 5,
+            "account_id": 10000,
+            
+            "first_name": "Goldie",
+            "last_name": "Wilson",
+            "email": "goldiewilson@hillvalleycalifornia.gov",
+          },
+          {
+            "id": 5475,
+            "login_id": 5,
+            "account_id": 12345,
+            
+            "first_name": "Goldenrod",
+            "last_name": "Wilson, IV",
+            "email": "goldiewilson@gmail.com",
+          }
+        ],
+        "accounts": [
+          {
+            "id": 10000,
+            
+            "company": "ABC Company",
+            
+          },
+          {
+            "id": 12345,
+            
+            "company": "123 Company",
+            
+          }
+        ]
+      }
 
 
 });
